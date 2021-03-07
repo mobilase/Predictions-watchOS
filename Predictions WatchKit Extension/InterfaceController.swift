b67af569915773720df2c16c69d87dc32f7861aa
Sergey Lavrov <lavrov-sergey@yandex.ru>
2021-03-07 23:05:31 +0300

    Create little app for ask question and get answer

--- Predictions WatchKit Extension/InterfaceController.swift
+++ Predictions WatchKit Extension/InterfaceController.swift
@@ -10,17 +10,11 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
-    override func awake(withContext context: Any?) {
-        // Configure interface objects here.
-    }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-    }
+    @IBOutlet weak var textLabel: WKInterfaceLabel!
+    let array = ["Да", "Нет", "Спроси позже", "Скорее да, чем нет", "Да нет наверное", "Возможно", "Конечно", "Не знаю"]
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
+    @IBAction func actionButton() {
+        textLabel.setText(array[Int.random(in: 0...array.count - 1)])
     }
-
 }
