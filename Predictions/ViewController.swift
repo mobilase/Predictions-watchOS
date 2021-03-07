7ecb2d5186ebc56127a8419fb6ade58c53977be1
Sergey Lavrov <lavrov-sergey@yandex.ru>
2021-03-07 22:39:10 +0300

    Initial Commit

---
+++ Predictions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Predictions
+//
+//  Created by Сергей Лавров on 07.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
